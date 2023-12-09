908637ec03bb412f5ce300c2786c328707b61ce3
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-12-09 10:42:06 +0300

    Initial Commit

---
+++ SwiftUI-HandyStuff/SwiftUI_HandyStuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HandyStuffApp.swift
+//  SwiftUI-HandyStuff
+//
+//  Created by Samet Çağrı Aktepe on 9.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HandyStuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
