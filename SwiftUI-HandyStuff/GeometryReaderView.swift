4310e83d0158c00a78fa5913c2ed79936b050800
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-12-09 11:07:52 +0300

    Update GeometryReaderView.swift

--- SwiftUI-HandyStuff/GeometryReaderView.swift
+++ SwiftUI-HandyStuff/GeometryReaderView.swift
@@ -8,16 +8,32 @@
 import SwiftUI
 
 struct GeometryReaderView: View {
+    
+    func getPercentage(geo: GeometryProxy) -> Double {
+        // center of the screen
+        let maxDistance = UIScreen.main.bounds.width / 2
+        // current X position
+        let currentX = geo.frame(in: .global).midX
+        // calculate percentage
+        return Double(1 - (currentX / maxDistance))
+    }
+    
     var body: some View {
-        GeometryReader { geometry in
-            HStack(spacing: 0) {
-                Rectangle()
-                    .fill(.red)
-                    .frame(width: geometry.size.width * 0.666)
-                Rectangle()
-                    .fill(.green)
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(0..<20) { index in
+                    GeometryReader { geometry in
+                        RoundedRectangle(cornerRadius: 20)
+                            .rotation3DEffect(
+                                .degrees(getPercentage(geo: geometry) * 40),
+                                axis: (x: 0.0, y: 1.0, z: 0.0)
+                            )
+                    }
+                    .frame(width: 300, height: 250)
+                    .padding()
+                    
+                }
             }
-            .ignoresSafeArea()
         }
         
     }
