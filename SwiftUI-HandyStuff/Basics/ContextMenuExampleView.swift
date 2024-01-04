9d7bae70e1bca179b7b2e130ba0c38fd2803e6cd
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2024-01-04 10:46:09 +0300

    Enhancement ContextMenu

---
+++ SwiftUI-HandyStuff/Basics/ContextMenuExampleView.swift
@@ -0,0 +1,60 @@
+//
+//  ContextMenuExampleView.swift
+//  SwiftUI-HandyStuff
+//
+//  Created by Samet Çağrı Aktepe on 4.01.2024.
+//
+
+import SwiftUI
+
+struct ContextMenuExampleView: View {
+    
+    @State private var backgroundColor: Color = Color.purple
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10.0) {
+            Image(systemName: "house.fill")
+                .font(.title)
+            
+            Text("Çağrı Aktepe")
+                .font(.headline)
+            
+            Text("How to use Context Menu")
+                .font(.subheadline)
+        }
+        .foregroundStyle(.white)
+        .padding(30)
+        .background(
+            backgroundColor
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+        )
+        .contextMenu(ContextMenu(menuItems: {
+            
+            Button {
+                backgroundColor = .blue
+            } label: {
+                Label("Share", systemImage: "flame.fill")
+            }
+
+            Button {
+                backgroundColor = .red
+            } label: {
+                Text("Report")
+            }
+            
+            Button {
+                backgroundColor = .yellow
+            } label: {
+                HStack {
+                    Text("Like")
+                    Spacer()
+                    Image(systemName: "heart.fill")
+                }
+            }
+        }))
+    }
+}
+
+#Preview {
+    ContextMenuExampleView()
+}
