860db33c8405281c298d9dc2352bb0b5a0f644f3
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2024-01-12 17:33:14 +0300

    Enhancement TextSelection

---
+++ SwiftUI-HandyStuff/Basics/TextSelectionExampleView.swift
@@ -0,0 +1,19 @@
+//
+//  TextSelectionExampleView.swift
+//  SwiftUI-HandyStuff
+//
+//  Created by Samet Çağrı Aktepe on 12.01.2024.
+//
+
+import SwiftUI
+
+struct TextSelectionExampleView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    TextSelectionExampleView()
+}
